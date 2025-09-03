e5daeaefecdd01948dc1336f556c15548b694d31
caitl <wils1353@purdue.edu>
2025-09-03 00:03:50 -0400

    Springboot Backend - No User Stories

---
+++ src/main/java/com/example/dominaitionbackend/DominAItionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dominaitionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominAItionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DominAItionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dominaitionbackend/DominAItionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dominaitionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominAItionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
